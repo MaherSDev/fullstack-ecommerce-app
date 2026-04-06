66fc992eff077830cd3aadf3b4d40f54bebcc65c
MaherSDev <mahersnobar1@gmail.com>
2026-04-06 09:17:42 +0300

    Create Shpping cart drawer

---
+++ src/app/features/globalSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+
+interface IGlobalState {
+  isOpenCartDrawer: boolean;
+  onOpenCartDrawer: boolean;
+  onCloseCartDrawer: boolean;
+}
+
+const initialState: IGlobalState = {
+  isOpenCartDrawer: false,
+  onOpenCartDrawer: false,
+  onCloseCartDrawer: false,
+};
+
+const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {
+    isOpenCartDrawer: (state) => {
+      state.isOpenCartDrawer = !state.isOpenCartDrawer;
+    },
+    onOpenCartDrawerAction: (state) => {
+      state.onOpenCartDrawer = true;
+      state.isOpenCartDrawer = true;
+    },
+    onCloseCartDrawerAction: (state) => {
+      state.onCloseCartDrawer = false;
+      state.isOpenCartDrawer = false;
+    },
+  },
+});
+
+export const {
+  isOpenCartDrawer,
+  onCloseCartDrawerAction,
+  onOpenCartDrawerAction,
+} = globalSlice.actions;
+export const selectGlobal = ({ global }: RootState) => global;
+export default globalSlice.reducer;
